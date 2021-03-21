d9bef3e8f9265e097811ae6ca8b96eab76ff73c3
Elena Fayle <efayle@my.wctc.edu>
2021-03-21 15:42:12 -0500

    Bug/Defect - User Input

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace Midterm
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            string ticketFilePath = Directory.GetCurrentDirectory() + "\\Tickets.csv";
+            TicketFile ticketFile = new TicketFile(ticketFilePath);
+
             logger.Info("Program started");
 
             string userChoice;
@@ -17,7 +20,9 @@ namespace Midterm
                 userChoice = Console.ReadLine();
 
                 if (userChoice == "1") {
-                    Console.WriteLine("Display all tickets");
+                    foreach (System s in ticketFile.Systems) {
+                        Console.WriteLine(s.Display());
+                    }
                 } else if (userChoice == "2") {
                     Console.WriteLine("Create a ticket");
 
@@ -28,7 +33,48 @@ namespace Midterm
                         userOption = Console.ReadLine();
 
                         if (userOption == "1") {
+                            //add ticket
+                            Tickets tickets = new Tickets();
+
+                            //ask user 
                             Console.WriteLine("Bug/Defect");
+                            Console.WriteLine("Enter the following");
+
+                            Console.WriteLine("ID:");
+                            tickets.ID = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Summary:");
+                            tickets.summary = Console.ReadLine();
+
+                            Console.WriteLine("Status:");
+                            tickets.status = Console.ReadLine();
+
+                            Console.WriteLine("Priority:");
+                            tickets.priority = Console.ReadLine();
+
+                            Console.WriteLine("Submitter:");
+                            tickets.submitter = Console.ReadLine();
+
+                            Console.WriteLine("Assigner:");
+                            tickets.assigner = Console.ReadLine();
+
+                            string watchers;
+                            do {
+                                Console.WriteLine("Watchers (Enter 'done' when fininshed)");
+                                watchers = Console.ReadLine();
+
+                                if (watchers != "done" && watchers.Length > 0) {
+                                    tickets.watchers.Add(watchers);
+                                } else if (tickets.watchers.Count == 0) {
+                                    tickets.watchers.Add("(No watchers listed)");
+                                }
+
+                            } while (watchers != "done");
+
+                            Console.WriteLine("Severity:");
+                            tickets.severity = Console.ReadLine();
+
+                            //add ticket
+                            ticketFile.AddTicket(tickets);
                         } else if (userOption == "2") {
                             Console.WriteLine("Enhancement");
                         } else if (userOption == "3") {
